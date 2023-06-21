8a991a63e1a639f891bdb38ba65e31ed561fab99
Pe4all <shefov.max@yandex.ru>
2023-06-21 03:16:37 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ function Form() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [text, setText] = useState('');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [option, setOption] = useState('Dropdown option');
+  const [radio, setRadio] = useState('');
+  const [toggle, setToggle] = useState(false);
+  const [remembered, setRemembered] = useState(false);
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (name && password && text) {
+    if (name && password && text && option && radio)  {
       const formData = {
-        name: name,
+        userName: name,
         password: password,
-        text: text
+        text: text,
+        remembered: remembered,
+        toggeled: toggle,
+        radioSelection: radio,
+        dropdownTitle: option,
       };
-
+      console.log(formData);
       alert(JSON.stringify(formData));
     } else {
       alert('Заполните все поля формы');
     }
   };
+
+  const isPasswordValid = ( password.length >= 4 && password.length <= 12 )|| password.length === 0;
+  const isNameValid = ( name.length >= 3 || name.length === 0 ) && /\d/.test(name) !== true;
+  const isTextValid = text.length > 8 || text.length === 0;
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+  const handleOption = (e) => {
+    setOption(e.target.getAttribute('value'));
+  }
+  const handleRadio = (e) => {
+    setRadio(e.target.getAttribute('value'));
+  }
 
   return (
     <div className="container">
@@ -27,26 +50,32 @@ function Form() {
         <div className="container__item">
           <label>Username</label>
           <input
-            maxlength="20"
+            pattern="[\D\s]{3,}"
+            maxLength="20"
             type="text"
             id="name"
             placeholder="Enter username"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+
+          {isNameValid ? ( ''
+          ) : (<p className="textMini textRed">Error message informing me of a problem</p>)}
         </div>
         <div className="container__item">
           <label>Password</label>
           <input
-            minlength="4"
-            maxlength="12"
+            minLength="4"
+            maxLength="12"
             type="password"
             id="password"
             placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <p className='textMini'>Your password is between 4 and 12 characters</p>
+          {isPasswordValid ? (
+            <p className="textMini">Your password is between 4 and 12 characters</p>
+          ) : (<p className="textMini textRed">Error message informing me of a problem</p>)}
         </div>
         <div className="container__item">
           <label>Input Text Label</label>
@@ -54,53 +83,64 @@ function Form() {
             type="text"
             id="text"
             placeholder="Type here"
+            minLength="9"
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
+          {isTextValid ? (
+            <p></p>
+          ) : (<p className="textMini textRed">Error message informing me of a problem</p>)}
         </div>
-        <div className='container__item'>
-          <label for="check">
-            <input type="checkbox" id="check" className='check' />
+        <div className='container__item heightFix'>
+          <label>
+            <input type="checkbox" id="check" className='check' onChange={() => setRemembered(!remembered)}/>
             <div></div>
             <p>Remember me</p>
           </label>
         </div>
         <div className='container__item'>
-          <label class="switch">
-            <input type="checkbox" defaultChecked />
-            <span class="slider round"></span>
-            <p>Off</p>
+          <label className="switch">
+            <input type="checkbox" 
+            onChange={() => setToggle(!toggle)}
+            />
+            <span className="slider round"></span>
+            {toggle ? (
+            <p> On </p>
+          ) : (<p>Off</p>)}
           </label>
         </div>
-        <div class="checkbox-wrapper">
+        <div className="checkbox-wrapper">
           <label>
-            <input type="radio" class="modern-radio" value="1" name="a" />
+            <input type="radio" className="modern-radio" onClick={handleRadio} value="1" name="a" />
             <span></span>
             <p>Radio selection 1</p>
           </label>
           <label>
-            <input type="radio" class="modern-radio" value="2" name="a" />
+            <input type="radio" className="modern-radio" onClick={handleRadio} value="2" name="a" />
             <span></span>
             <p>Radio selection 2</p>
           </label>
           <label>
-            <input type="radio" class="modern-radio" value="3" name="a" />
+            <input type="radio" className="modern-radio" onClick={handleRadio} value="3" name="a" />
             <span></span>
             <p> Radio selection 3</p>
           </label>
         </div>
         <div className="container__item">
-          <label>Dropdown title</label>
-          <span class="custom-dropdown big">  
-          <select>   
-            <option>Dropdown option</option>  
-            <option>Dropdown option 1</option>   
-            <option>Dropdown option 2</option>  
-          </select> 
-          </span> 
+          <label>Dropdown Title</label>
+          <span className="custom-dropdown">
+            <ul
+              className={`menu ${isDropdownOpen ? 'open' : ''}`}
+              onClick={handleDropdownToggle}
+            ><span>{option}</span>
+              <li onClick={handleOption} value="Dropdown option">Dropdown option</li>
+              <li onClick={handleOption} value="Dropdown option 1">Dropdown option 1</li>
+              <li onClick={handleOption} value="Dropdown option 2"> Dropdown option 2</li>
+            </ul>
+          </span>
         </div>
         <div className='container__buttons'>
-          <button className='cancel' type="reset">Cancel</button>
+          <button className='cancel' type="reset" onClick={() => setToggle(false)}>Cancel</button>
           <button className='next' type="submit">Next</button>
         </div>
       </form>
